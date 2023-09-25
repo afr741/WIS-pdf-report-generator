86e6f1bcc633d282f4cb0575e4a984894cbd5b2f
Anushervon Rakhmat <afr741@mun.ca>
2023-09-25 13:58:54 +0500

    feat(pages): add components for each page

--- wis-pdf-generator-app/src/app/app.module.ts
+++ wis-pdf-generator-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './upload/upload.component';
+import { PdfComponent } from './pdf/pdf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UploadComponent,
+    PdfComponent
   ],
   imports: [
     BrowserModule,
