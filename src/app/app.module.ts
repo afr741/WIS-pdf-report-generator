3f5b53ec4e84671de1e3769150b899c6cdb2aaa4
Anushervon Rakhmat <afr741@mun.ca>
2023-09-27 18:03:44 +0500

    feat(upload): add upload page fields and write functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UploadComponent } from './upload/upload.component';
 import { PdfComponent } from './pdf/pdf.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    UploadComponent,
-    PdfComponent
-  ],
+  declarations: [AppComponent, LoginComponent, UploadComponent, PdfComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAuthenticatorModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { APIService, Report } from '../API.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+/** Subscription type will be inferred from this library */
+import { ZenObservable } from 'zen-observable-ts';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
-export class UploadComponent {
+export class UploadComponent implements OnInit, OnDestroy {
+  public createForm: FormGroup;
+  public reports: Array<Report> = [];
 
+  constructor(private api: APIService, private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      name: ['', Validators.required],
+      testLocation: ['', Validators.required],
+      reportNum: ['', Validators.required],
+      lotNum: ['', Validators.required],
+      customerName: ['', Validators.required],
+      origin: ['', Validators.required],
+      stations: ['', Validators.required],
+      variety: ['', Validators.required],
+    });
+  }
+
+  private subscription: ZenObservable.Subscription | null = null;
+
+  async ngOnInit() {
+    /* fetch reports when app loads */
+    this.api.ListReports().then((event) => {
+      this.reports = event.items as Report[];
+    });
+
+    /* subscribe to new restaurants being created */
+    this.subscription = this.api
+      .OnCreateReportListener()
+      .subscribe((event: any) => {
+        const newRestaurant = event.value.data.onCreateReport;
+        this.reports = [newRestaurant, ...this.reports];
+      });
+  }
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = null;
+  }
+
+  public onCreate(report: Report) {
+    this.api
+      .CreateReport(report)
+      .then(() => {
+        console.log('item created!');
+        this.createForm.reset();
+      })
+      .catch((e) => {
+        console.log('error creating report...', e);
+      });
+  }
 }
