c6dc9ce40f42ea971b153e8cfd54e0d6a12c9aba
Anushervon Rakhmat <afr741@mun.ca>
2023-10-26 01:37:51 +0500

    fix(pdf): fixed the fetching data

--- src/app/pdf/pdf.component.ts
+++ src/app/pdf/pdf.component.ts
@@ -18,30 +18,77 @@ export class PdfComponent implements OnInit {
   constructor(private api: APIService, public router: Router) {}
 
   async ngOnInit() {
-    /* fetch reports when app loads */
-    setTimeout(() => {
-      this.isLoading = false;
-      this.api
+    let pollingAttempts = 0;
+
+    const fetchData = async () => {
+      await this.api
         .ListReports()
         .then((event) => {
           this.reports = event.items as Report[];
           console.log('reports', this.reports);
-          //sort based on dates
           this.reports.sort((a, b) => {
             let dateA: any = new Date(a.updatedAt);
             let dateB: any = new Date(b.updatedAt);
-
             return dateB - dateA;
           });
-        })
-        .then(() => {
-          this.processPDFData();
         })
         .catch((err) => {
           console.log(err);
         });
-    }, 5000);
+      console.log('null reports', this.reports);
+      if (this.reports[0].dataRows == null) {
+        if (pollingAttempts < 10) {
+          // Limit the number of polling attempts
+          pollingAttempts++;
+          setTimeout(fetchData, 3000);
+        } else {
+          console.log('Data not available after 10 polling attempts.');
+          this.isLoading = false;
+          // Handle the case where data is not available.
+        }
+      } else {
+        this.isLoading = false;
+        this.processPDFData();
+      }
+    };
+
+    fetchData(); // Start the initial data fetch.
   }
+
+  // async ngOnInit() {
+  //   /* fetch reports when app loads */
+  //   const fetchReports = async () => {
+  //     try {
+  //       const response = await this.api.ListReports();
+  //       this.reports = response.items as Report[];
+  //       console.log('reports', this.reports);
+
+  //       // Sort based on dates
+  //       this.reports = this.reports.sort((a, b) => {
+  //         const dateA: any = new Date(a.updatedAt);
+  //         const dateB: any = new Date(b.updatedAt);
+  //         return dateB - dateA;
+  //       });
+
+  //       // Check if dataRows is available
+  //       if (this.reports.length > 0 && this.reports[0].dataRows !== null) {
+  //         console.log('check if', this.reports[0]);
+  //         this.processPDFData();
+  //       } else {
+  //         // Continue fetching reports
+  //         console.log('settime', this.reports);
+  //         setTimeout(fetchReports, 5000);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //       // Retry after a delay in case of an error
+  //       setTimeout(fetchReports, 5000);
+  //     }
+  //   };
+
+  //   // Initial call to start the recursive process
+  //   fetchReports();
+  // }
 
   processPDFData() {
     let data = this.reports[0].dataRows;
