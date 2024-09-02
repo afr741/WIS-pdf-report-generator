db6fa51e1f4c279488e04f8cbe5b10b57e163431
Anushervon Rakhmat <afr741@mun.ca>
2024-09-01 23:20:43 -0400

    fix: reset page layout

--- src/app/reset/reset.component.ts
+++ src/app/reset/reset.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
 import { AuthService } from '../AuthService';
 
 @Component({
